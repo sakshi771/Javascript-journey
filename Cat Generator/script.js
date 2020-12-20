28c85631866ff8d35c0800cb0bae7c77341721f3
Sakshi sapkale <sakshisapkale77@gmail.com>
2020-12-20 23:53:55 +0530

    Add cat generator

---
+++ Cat Generator/script.js
@@ -0,0 +1,7 @@
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src ="https://media.tenor.com/images/1a5a73dbaaaf7ec3f0da44fb75e58c76/tenor.gif";
+    div.appendChild(image);
+}
